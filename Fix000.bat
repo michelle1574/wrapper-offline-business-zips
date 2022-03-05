5d134d6629b40402bae97ee3cc1b49a5675c6c36
Michael <70703680+michelle1574@users.noreply.github.com>
2022-03-05 20:32:46 +0100

    Fix something (DOWNLOAD AND RUN IF YOU DOWNLOADED RELEASE)

--- Fix000.bat
+++ Fix000.bat
@@ -1,6 +1,6 @@
 @echo off
-if not exists start_wrapper.bat goto nostart
 cd /d "%~dp0"
+if not exists start_wrapper.bat goto nostart
 move /Y server\static\go server\static\ad44370a650793d9\go
 pause
 exit
